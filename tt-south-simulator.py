cee984e4cdab6257bf0b0ed7f369b0412eaae673
Johan Paz <japaz@tid.es>
2015-01-21 19:59:18 +0100

    First complete and functional version. 

--- tt-south-simulator.py
+++ tt-south-simulator.py
@@ -2,8 +2,50 @@
 # -*- coding: utf-8 -*-
 # @Author: japaz
 from lib.config import Environment
-from os import path
+import lib.sender as sender
+from os import path,walk
+import sys
 
-context = Environment(path.join(path.dirname(__file__),"conf","environment.yaml"))
+def execute_sequence(sequence,context,verbosity):
+	print "Executing the sequence:", sequence
+	sender.send_sequence(sequence,context,verbosity)
 
+def execute_all_in(origin,context,verbosity):
+	for dirname, dirnames, filenames in walk(origin):
+	    # print path to all filenames.
+	    for filename in filenames:
+	    	if filename.endswith('.yaml'):
+	    		execute_sequence(filename[:-5],context,verbosity)
+
+doall = False
+verbosity = False
+environment = 'default'
+sequence = None
+
+num_param = len(sys.argv)
+if num_param < 2:
+    print "Usage: python tt-south-simulator.py [--V] [--environment] all|name-sequence"
+else:
+	for i in range(1,num_param):
+		arg = sys.argv[i]
+		if arg == 'all':
+			doall = True
+		elif arg == '--V':
+			verbosity = True
+		elif arg.startswith('--'):
+			environment = arg[2:]
+		else:
+			sequence = arg
+
+context = Environment(path.join(path.dirname(__file__),"conf","environment.yaml"),environment,True)
+
+if not doall and not sequence:
+	print "ERROR: no sequence to execute selected."
+	sys.exit(1)
+
+if not doall:
+	execute_sequence(sequence,context,verbosity)
+else:
+	source = path.join(path.dirname(__file__),"samples")
+	execute_all_in(source,context,verbosity)
 
